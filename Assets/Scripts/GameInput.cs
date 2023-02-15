f5190aef236a3264db5e4454af16b830ad6002d5
Santiago Sala <pipo0716@gmail.com>
2023-02-15 12:14:30 +0200

    Added new input system package (not yet used)

---
+++ Assets/Scripts/GameInput.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInput : MonoBehaviour
+{
+   public Vector2 GetMovementVectorNormalized()
+    {
+        Vector2 inputVector = new Vector2(0, 0);
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector.y = +1;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y = -1;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x = -1;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x = +1;
+        }
+
+        inputVector = inputVector.normalized;
+
+        return inputVector;
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,39 +8,19 @@ public class Player : MonoBehaviour
     private float _moveSpeed = 7f;
     [SerializeField]
     private float _rotateSpeed = 10f;
+    [SerializeField]
+    private GameInput gameInput;
 
     private bool isWalking;
 
     private void Update()
     {
-        Vector2 inputVector = new Vector2(0, 0);
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = +1;
-        }
-        
-        if(Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-        }
-        
-        if(Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-        }
-        
-        if(Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = +1;
-        }
 
-        inputVector = inputVector.normalized;
-
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized(); 
         Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
 
 
         //isWalking = moveDirection != Vector3.zero;
-
         if (moveDirection == Vector3.zero)
         {
             isWalking = false;
