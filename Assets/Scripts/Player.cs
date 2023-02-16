b6cbed213db1df881e8d420a178bef700eccdda6
Santiago Sala <pipo0716@gmail.com>
2023-02-16 10:05:33 +0200

    Added a capsule  cast for the player to detect obstacles

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -19,6 +19,49 @@ public class Player : MonoBehaviour
         Vector2 inputVector = gameInput.GetMovementVectorNormalized(); 
         Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
 
+        float moveDistance = _moveSpeed * Time.deltaTime;
+        float playerRadius = 0.7f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, 
+            playerRadius,  moveDirection, moveDistance);
+
+        if(!canMove)
+        {
+            // Trying to move diagonally
+
+            // Attemp to move on the x axis 
+            Vector3 moveDirection_X = new Vector3(moveDirection.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight,
+            playerRadius, moveDirection_X, moveDistance);
+
+            if (canMove)
+            {
+                // Move on the X axis
+                moveDirection = moveDirection_X;
+            }
+            else
+            {
+                // X axis not possible. Attemp to move on the Z axis
+                Vector3 moveDirection_Z = new Vector3(0, 0, moveDirection.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight,
+                playerRadius, moveDirection_Z, moveDistance);
+
+                if (canMove)
+                {
+                    // Move to Z axis.
+                    moveDirection = moveDirection_Z;
+                }
+                else
+                {
+                    //Cannot move diagonally to any direction
+                }
+            }
+
+        }
+        if (canMove)
+        {
+            transform.position += moveDirection * moveDistance;  
+        }
 
         //isWalking = moveDirection != Vector3.zero;
         if (moveDirection == Vector3.zero)
@@ -32,7 +75,6 @@ public class Player : MonoBehaviour
 
         transform.forward = Vector3.Slerp(transform.forward, moveDirection, Time.deltaTime * _rotateSpeed);
 
-        transform.position += moveDirection * _moveSpeed * Time.deltaTime;
 
     }
 
